f0258019b5b78f61a655e509f73a92bfc98d7512
Anonimous05 <66255360+Anonimous05@users.noreply.github.com>
2021-01-21 19:25:23 +0600

    Add files via upload

---
+++ change_vars.py
@@ -0,0 +1,8 @@
+x = 10
+y = 120
+print('X: ' + str(x),'Y: ' + str(y))
+e = x
+x = y
+y = e
+print('X: ' + str(x),'Y: ' + str(y))
+
---
+++ primitive_calculator.py
@@ -0,0 +1,8 @@
+a1 = int(input("Введите 1 число: "))
+a2 = int(input("Введите 2 число: "))
+a3 = int(input("Введите 3 число: "))
+a4 = int(input("Введите 4 число: "))
+
+otvet = a1 + a2 + a3 + a4
+
+print(str(a1) + "+" + str(a2) + "+" + str(a3) + "+" + str(a4) + " = " + str(otvet))
